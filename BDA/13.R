5f41113c7d7a887f97cf36b8e7ccef5fa7d44a54
Vighnesh Adelkar <vighnesh.adelkar21@gmail.com>
2024-10-19 19:52:47 +0530

    Update 13.R

--- BDA/13.R
+++ BDA/13.R
@@ -1,3 +1,8 @@
+# i. Create a subset of course less than 3 by using [ ] brackets and demonstrate the
+# output.
+# ii. Create a subset where the course column is less than 3 or the class equals to 2
+# by using subset () function and demonstrate the output.
+
 data=data.frame(
     course=c(1,2,3,4,5,6),
     id=c(11,12,13,14,15,16),
