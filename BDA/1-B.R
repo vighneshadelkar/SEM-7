96527d199ea07c127540a1a93e95912d0330572d
Vighnesh Adelkar <vighnesh.adelkar21@gmail.com>
2024-10-19 19:49:47 +0530

    Update 1-B.R

--- BDA/1-B.R
+++ BDA/1-B.R
@@ -1,3 +1,6 @@
+# (B) Write a R program to create a Data Frame which contain details of 5 employees and display
+# summary of the data using R.
+
 data<- data.frame(
 Category= c("A","B","D","E","F"),
 Age= c(10,15,20,30,5),
