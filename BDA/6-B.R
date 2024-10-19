386a27b6d305db8f917e4c8123ca5c3bd52c339a
Vighnesh Adelkar <vighnesh.adelkar21@gmail.com>
2024-10-19 19:52:03 +0530

    Update 6-B.R

--- BDA/6-B.R
+++ BDA/6-B.R
@@ -1,3 +1,9 @@
+# i) Which R command will Mr. John use to enter these values demonstrate the output.
+# ii) Now Mr. John wants to add the salaries of 5 new employees in the existing table,
+# which command he will use to join datasets with new values in R. Demonstrate the
+# output.
+# (iii) Visialize the data using chart .
+
 salaries <- data.frame(
   name = c("John", "Mary", "David", "Emily", "James"),
   salary = c(50000, 60000, 70000, 80000, 90000) 
