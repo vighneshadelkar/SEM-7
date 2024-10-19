83437ae9b55351d52adb6eb14e28c138e11d099d
Vighnesh Adelkar <vighnesh.adelkar21@gmail.com>
2024-10-19 20:00:46 +0530

    Update 14.R

--- BDA/14.R
+++ BDA/14.R
@@ -1,10 +1,17 @@
+# 1) create data frame for followinf 4 vectors
+# 2) display structure and summary of above data
+# 3) extract emp_name and salary from the anove data structure
+# 4) extract the employee details whose salary is less than or equal to 60000
+
 emp_id=c(1:5)
 emp_name=c("a","b","c","d","e")
 start_date=c(1,1,1,1,1)
 salary=c(60000,45000,75000,84000,200000)
 
 data=data.frame(emp_id,emp_name,start_date,salary)
-print(data)
+
+summarized_data=summary(data)
+print(summarized_data)
 
 print(data$emp_id)
 print(data$emp_name)
