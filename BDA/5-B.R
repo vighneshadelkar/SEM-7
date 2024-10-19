a44ff576c509c47b25a2f079fa659e3397e1feb8
Vighnesh Adelkar <vighnesh.adelkar21@gmail.com>
2024-10-19 19:51:15 +0530

    Update 5-B.R

--- BDA/5-B.R
+++ BDA/5-B.R
@@ -1,3 +1,7 @@
+# (i) Create a subset of subject less than 4 by using subset () funcon and demonstrate the output.
+# (ii) Create a subject where the subject column is less than 3 and the class equals to 2 by using []
+# brackets and demonstrate the output using R
+
 data<-data.frame(
     Subject=c(1,2,3,4,5,6),
     Class=c(1,2,1,2,1,2),
