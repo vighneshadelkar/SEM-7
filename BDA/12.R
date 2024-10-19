a3945d95a69daf3c48ab63058b28b122b8845899
Vighnesh Adelkar <vighnesh.adelkar21@gmail.com>
2024-10-19 19:52:26 +0530

    Update 12.R

--- BDA/12.R
+++ BDA/12.R
@@ -1,3 +1,10 @@
+# Which function is used to concatenate text values in R. Write a script to concatenate text and
+# numerical values in R.
+# Text 1: Ram has scored
+# Text 2: 89
+# Text 3: marks
+# Text 4: in Mathematics
+
 text1="ram has scored"
 text2=89
 text3="marks"
