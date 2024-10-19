d649c485fe027e4a9f20cc1c4bf452923b79784a
Vighnesh Adelkar <vighnesh.adelkar21@gmail.com>
2024-10-19 20:02:19 +0530

    Update 15.R

--- BDA/15.R
+++ BDA/15.R
@@ -1,6 +1,8 @@
+# demonstrate the function with the input values and write the output to combine data from two data sets into another dataset c
+
 dataA=c(6,7,8,9)
 dataB=c(1,2,4,5)
 
-combined_data=c(dataA,dataB)
+dataC=c(dataA,dataB)
 
-print(combined_data)
+print(dataC)
