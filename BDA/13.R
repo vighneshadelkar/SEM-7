b53bb8f9fe1df2a56fabaa43e7ee8b3becf15d41
Vighnesh Adelkar <vighnesh.adelkar21@gmail.com>
2024-10-19 19:40:49 +0530

    Create 13.R

---
+++ BDA/13.R
@@ -0,0 +1,12 @@
+data=data.frame(
+    course=c(1,2,3,4,5,6),
+    id=c(11,12,13,14,15,16),
+    class=c(1,2,1,2,1,2),
+    marks=c(56,75,48,69,84,53)
+)
+
+course_subset=data[data$course<3,] 
+
+course_subset2=subset(data,course<3 | class==2)
+print(course_subset)
+print(course_subset2)
