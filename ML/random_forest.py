4fa5c65a7cb8ae01abda4c6e8a6c50916604d8c1
Vighnesh Adelkar <vighnesh.adelkar21@gmail.com>
2024-10-23 16:10:02 +0530

    Update random_forest.py

--- ML/random_forest.py
+++ ML/random_forest.py
@@ -11,7 +11,7 @@ y = iris.target
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-model=RandomForestClassifier(n_estimators==100, random_state=423)
+model=RandomForestClassifier(n_estimators=100, random_state=42)
 model.fit(X_train,y_train)
 
 # 4. Make predictions on the test set
