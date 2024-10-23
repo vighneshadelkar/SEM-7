9c1f1ae2960c5318a3b4d6100d1fc238d0c6767d
Vighnesh Adelkar <vighnesh.adelkar21@gmail.com>
2024-10-23 16:06:29 +0530

    Create bagging.py

---
+++ ML/bagging.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import BaggingClassifier, AdaBoostClassifier, RandomForestClassifier, GradientBoostingClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, f1_score
+from sklearn.datasets import load_iris
+
+iris = load_iris()
+X = iris.data 
+y = iris.target 
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model=BaggingClassifier(n_estimators==100, random_state=423)
+model.fit(X_train,y_train)
+
+# 4. Make predictions on the test set
+y_pred_test= model.predict(X_test)
+
+print(y_pred_test)
+# 5. Evaluate the model
+test_acc = accuracy_score(y_test, y_pred_test)
+precision = precision_score(y_test, y_pred_test, average='macro')
+recall = recall_score(y_test, y_pred_test, average='macro')
+f1 = f1_score(y_test, y_pred_test, average='macro')
+conf_matrix = confusion_matrix(y_test, y_pred_test)
+
+# Output the evaluation metrics
+print(f"Test Accuracy: {test_acc * 100:.2f}%")
+print(f"Precision: {precision * 100:.2f}%")
+print(f"Recall: {recall * 100:.2f}%")
+print(f"F1-Score: {f1 * 100:.2f}%")
+print("\nConfusion Matrix:")
+print(conf_matrix)
