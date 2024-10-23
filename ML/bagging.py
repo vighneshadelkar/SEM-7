fe5e9e9799a1960428d23e4ab10d261f7538c87d
Vighnesh Adelkar <vighnesh.adelkar21@gmail.com>
2024-10-23 16:10:24 +0530

    Update bagging.py

--- ML/bagging.py
+++ ML/bagging.py
@@ -12,7 +12,7 @@ y = iris.target
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-model=BaggingClassifier(n_estimators==100, random_state=423)
+model=BaggingClassifier(n_estimators=100, random_state=42)
 model.fit(X_train,y_train)
 
 # 4. Make predictions on the test set
